8c4fb9405fc2b24feeafca499d3dfa25e419e66b
U-Rob-Desktop\Rob <Rob@Rob-Desktop.(none)>
2013-08-21 21:21:38 -0400

    remove unncessary src files

--- src/com/example/quizapp/Question.java
+++
@@ -1,8 +0,0 @@
-package com.example.quizapp;
-
-abstract class Question {
-
-	public int questionType;
-	public String questionText;
-	
-}
--- src/com/example/quizapp/QuestionType.java
+++
@@ -1,6 +0,0 @@
-package com.example.quizapp;
-
-public enum QuestionType {
-	MultipleChoice
-
-}
